5bfe005dc924dcd7c057c75ce3c392916b3714bf
Showrov <yshowrov7@gmail.com>
2025-09-29 23:10:23 +0600

    next auth with google

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/components/modules/Auth/LoginCard'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <LoginForm />
+    </div>
+  )
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/components/modules/Auth/RegisterCard'
+import React from 'react'
+
+export default function RegisterPage() {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
